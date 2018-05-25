9934f650f0235d33c1f920d135f33f8944ec9258
jack <ac861219@gmail.com>
2018-05-25 14:22:18 +0800

    support swift 4.x

--- SMIconLabel/SMIconLabel.swift
+++ SMIconLabel/SMIconLabel.swift
@@ -69,7 +69,7 @@ open class SMIconLabel: UILabel {
         var newRect = CGRect.zero
         let size = text.boundingRect(with: CGSize(width: frame.width - icon.size.width - iconPadding, height: CGFloat.greatestFiniteMagnitude),
                                      options: NSStringDrawingOptions.usesLineFragmentOrigin,
-                                     attributes: [ NSFontAttributeName : font ],
+                                     attributes: [ kCTFontAttributeName as NSAttributedStringKey : font ],
                                      context: nil).size
 
         guard let iconView = iconView else { return }
